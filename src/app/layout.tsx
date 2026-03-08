6d3a71c7284589883d0fd0d02409a0463eaa924e
Vibecode Terminal <terminal@vibecodeapp.com>
2026-03-08 17:52:45 +0000

    Sandbox autosave

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
+import { AnalyticsProvider } from "@/components/AnalyticsProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster position="top-right" richColors />
-        {children}
+        <AnalyticsProvider>
+          <Toaster position="top-right" richColors />
+          {children}
+        </AnalyticsProvider>
       </body>
     </html>
   );
